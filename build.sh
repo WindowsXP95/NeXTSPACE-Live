4216580eb8ff4e6832e605150d0c50c145d653d0
Jaiden Nowak <windowsxp519@gmail.com>
2022-01-19 19:32:42 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-livecd-creator --config nextspace.ks --title "nextspaceos-1.0_r1" date +%Y/%m/%d/%H%M%S
-"" --fslabel "NEXTSPACEOS"
+livecd-creator --config nextspace.ks --title "nextspaceos-1.0_r1" --fslabel "NEXTSPACEOS"
