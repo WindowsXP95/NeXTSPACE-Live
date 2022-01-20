203d17b1890ff9e5e5738a67a60ccc9cfb41eb9f
Jaiden Nowak <windowsxp519@gmail.com>
2022-01-20 14:02:17 -0500

    Update create.sh

--- Docker/centos7/create.sh
+++ Docker/centos7/create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t nextspace-dev .
+docker build -t nextspace-dev
 docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-dev nextspace-dev:latest
 docker start nextspace-dev
 docker exec -w / -it nextspace-dev git clone https://github.com/WindowsXP95/NeXTSPACE-Live.git
