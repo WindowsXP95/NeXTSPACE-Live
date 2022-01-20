e500fcb1c6fed94f510c6a2a962a89a5ffd5982a
Jaiden Nowak <windowsxp519@gmail.com>
2022-01-19 19:31:36 -0500

    added build date string (experimental)

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-livecd-creator --config nextspace.ks --title "nextspaceos-1.0_r1" --fslabel "NEXTSPACEOS"
+livecd-creator --config nextspace.ks --title "nextspaceos-1.0_r1" date +%Y/%m/%d/%H%M%S
+"" --fslabel "NEXTSPACEOS"
