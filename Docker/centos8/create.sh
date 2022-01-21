818bdb981c0bc9206db471589a553602c4dd9eba
Jaiden Nowak <windowsxp519@gmail.com>
2022-01-21 09:29:42 -0500

    updated for Cent OS beta

--- Docker/centos8/create.sh
+++ Docker/centos8/create.sh
@@ -2,8 +2,8 @@
 docker build -t nextspace-dev .
 docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-dev nextspace-dev:latest
 docker start nextspace-dev
-docker exec -w / -it nextspace-dev git clone https://github.com/nicktelindert/nextspace-build-iso.git
+docker exec -w / -it nextspace-dev git clone https://github.com/WindowsXP95/NeXTSPACE-Live.git
 docker exec -w /nextspace-build-iso -it nextspace-dev sh build.sh
-docker cp nextspace-dev:/nextspace-build-iso/NEXTSPACE.iso ~/NEXTSPACE.iso
+docker cp nextspace-dev:/NeXTSPACE-Live/Nextspaceos-beta.iso ~/Nextspaceos-beta.iso
 docker stop nextspace-dev
 docker container rm nextspace-dev
