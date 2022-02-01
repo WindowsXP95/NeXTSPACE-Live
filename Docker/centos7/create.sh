8ae9e92e6d1cdb10ea53ffa49e76d417d5a69847
Jaiden Nowak <windowsxp519@gmail.com>
2022-02-01 16:59:04 -0500

    minor changes

--- Docker/centos7/create.sh
+++ Docker/centos7/create.sh
@@ -3,7 +3,7 @@ docker build -t nextspace-dev .
 docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-dev nextspace-dev:latest
 docker start nextspace-dev
 docker exec -w / -it nextspace-dev git clone https://github.com/WindowsXP95/NeXTSPACE-Live.git
-docker exec -w /NeXTSPACE-Live -it nextspace-dev sh /v1/build.sh
+docker exec -w /NeXTSPACE-Live -it nextspace-dev sh build.sh
 docker cp nextspace-dev:/NeXTSPACE-Live/NEXTSPACEOS_v1.0.1.iso ~/NEXTSPACEOS_v1.0.1.iso
 docker stop nextspace-dev
 docker container rm nextspace-dev
--- v1/build.sh
+++
@@ -1,1 +0,0 @@
-livecd-creator --config nextspace_v2.ks --title "NEXTSPACEOS_v1.0.1" --fslabel "NEXTSPACEOS_v1.0.1"
