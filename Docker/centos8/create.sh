20de2dfb03305f7b88cc27093efbbb8de05fd219
Jaiden Nowak <windowsxp519@gmail.com>
2022-02-02 09:20:16 -0500

    Update create.sh

--- Docker/centos8/create.sh
+++ Docker/centos8/create.sh
@@ -4,6 +4,7 @@ docker create  -v /dev:/dev --privileged=true -ti  --name nextspaceos-dev nextsp
 docker start nextspaceos-dev
 docker exec -w / -it nextspaceos-dev git clone https://github.com/WindowsXP95/NeXTSPACE-Live.git
 docker exec -w /NeXTSPACE-Live -it nextspace-dev sh build_v2.sh
+docker echo " Finished Building iso. Now copying..."
 docker cp nextspaceos-dev:/NeXTSPACE-Live/NEXTSPACEOS_v2.iso ~/NEXTSPACEOS_v2.iso
 docker stop nextspaceos-dev
 #docker container rm nextspaceos-dev
