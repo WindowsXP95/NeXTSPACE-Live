9839ccc5c5be8d7ba9a6d91d26b5bd78d13b536f
Jaiden Nowak <windowsxp519@gmail.com>
2022-02-02 08:45:42 -0500

    Made v2 script a different docker env

--- Docker/centos8/create.sh
+++ Docker/centos8/create.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-docker build -t nextspace-dev .
-docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-dev nextspace-dev:latest
-docker start nextspace-dev
-docker exec -w / -it nextspace-dev git clone https://github.com/WindowsXP95/NeXTSPACE-Live.git
+docker build -t nextspaceos-dev .
+docker create  -v /dev:/dev --privileged=true -ti  --name nextspaceos-dev nextspaceos-dev:latest
+docker start nextspaceos-dev
+docker exec -w / -it nextspaceos-dev git clone https://github.com/WindowsXP95/NeXTSPACE-Live.git
 docker exec -w /NeXTSPACE-Live -it nextspace-dev sh build_v2.sh
-docker cp nextspace-dev:/NeXTSPACE-Live/NEXTSPACEOS_v2.iso ~/NEXTSPACEOS_v2.iso
-docker stop nextspace-dev
-
-#temp
-#docker container rm nextspace-dev
+docker cp nextspaceos-dev:/NeXTSPACE-Live/NEXTSPACEOS_v2.iso ~/NEXTSPACEOS_v2.iso
+docker stop nextspaceos-dev
+#docker container rm nextspaceos-dev
