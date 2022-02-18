52cc505b969d0d603830ec3eefa14de0dfc91a5a
Jaiden Nowak <windowsxp519@gmail.com>
2022-02-18 16:43:34 -0500

    bumped version from 1.0.2 to 1.0.3

--- Docker/centos7/create.sh
+++ Docker/centos7/create.sh
@@ -4,6 +4,6 @@ docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-dev nextspac
 docker start nextspace-dev
 docker exec -w / -it nextspace-dev git clone https://github.com/WindowsXP95/NeXTSPACE-OS.git
 docker exec -w /NeXTSPACE-OS -it nextspace-dev sh build.sh
-docker cp nextspace-dev:/NeXTSPACE-OS/NEXTSPACEOS_v1.0.2.iso ~/NEXTSPACEOS_v1.0.2.iso
+docker cp nextspace-dev:/NeXTSPACE-OS/NEXTSPACEOS_v1.0.3.iso ~/NEXTSPACEOS_v1.0.3.iso
 docker stop nextspace-dev
 docker container rm nextspace-dev
--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-livecd-creator --config nextspace.ks --title "NEXTSPACEOS_v1.0.2" --fslabel "NEXTSPACEOS_v1.0.2"
+livecd-creator --config nextspace.ks --title "NEXTSPACEOS_v1.0.2" --fslabel "NEXTSPACEOS_v1.0.3"
