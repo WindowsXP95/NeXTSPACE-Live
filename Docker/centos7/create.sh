c5595d4de71c095d293d1f7d7fbf3eebc50d14f2
Jaiden Nowak <windowsxp519@gmail.com>
2022-02-18 16:28:53 -0500

    Still updating repo names

--- Docker/centos7/create.sh
+++ Docker/centos7/create.sh
@@ -3,7 +3,7 @@ docker build -t nextspace-dev .
 docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-dev nextspace-dev:latest
 docker start nextspace-dev
 docker exec -w / -it nextspace-dev git clone https://github.com/WindowsXP95/NeXTSPACE-OS.git
-docker exec -w /NeXTSPACE-Live -it nextspace-dev sh build.sh
+docker exec -w /NeXTSPACE-OS -it nextspace-dev sh build.sh
 docker cp nextspace-dev:/NeXTSPACE-OS/NEXTSPACEOS_v1.0.2.iso ~/NEXTSPACEOS_v1.0.2.iso
 docker stop nextspace-dev
 docker container rm nextspace-dev
