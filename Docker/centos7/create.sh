860e7b2979296061cca9341197a39405039a9c91
Jaiden Nowak <windowsxp519@gmail.com>
2022-02-18 16:26:54 -0500

    New changes
    Entering renaming and fixing phase

--- Docker/centos7/create.sh
+++ Docker/centos7/create.sh
@@ -2,8 +2,8 @@
 docker build -t nextspace-dev .
 docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-dev nextspace-dev:latest
 docker start nextspace-dev
-docker exec -w / -it nextspace-dev git clone https://github.com/WindowsXP95/NeXTSPACE-Live.git
+docker exec -w / -it nextspace-dev git clone https://github.com/WindowsXP95/NeXTSPACE-OS.git
 docker exec -w /NeXTSPACE-Live -it nextspace-dev sh build.sh
-docker cp nextspace-dev:/NeXTSPACE-Live/NEXTSPACEOS_v1.0.2.iso ~/NEXTSPACEOS_v1.0.2.iso
+docker cp nextspace-dev:/NeXTSPACE-OS/NEXTSPACEOS_v1.0.2.iso ~/NEXTSPACEOS_v1.0.2.iso
 docker stop nextspace-dev
 docker container rm nextspace-dev
