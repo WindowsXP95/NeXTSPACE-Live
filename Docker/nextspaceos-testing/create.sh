fe938a3b91c5f36b85e226e96d368ce5b8b187ce
Jaiden Nowak <windowsxp519@gmail.com>
2022-02-18 20:37:39 -0500

    Update create.sh
    fixed a bug

--- Docker/nextspaceos-testing/create.sh
+++ Docker/nextspaceos-testing/create.sh
@@ -2,7 +2,7 @@
 docker build -t nextspace-testing .
 docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-testing nextspace-testing:latest
 docker start nextspace-testing
-docker exec -w / -it nextspace-testing git clone https://github.com/WindowsXP95/NeXTSPACE-Live.git
+docker exec -w / -it nextspace-testing git clone https://github.com/WindowsXP95/NeXTSPACE-OS.git
 docker exec -w /NeXTSPACE-OS -it nextspace-testing sh /NeXTSPACE-OS/testing/build_testing.sh
 docker cp nextspace-testing:/NeXTSPACE-OS/NEXTSPACEOS.iso ~/NEXTSPACEOS.iso
 docker stop nextspace-testing
