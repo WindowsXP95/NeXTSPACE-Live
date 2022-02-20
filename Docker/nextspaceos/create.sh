625f2e48651ef5ac169e11b44e1ae6d9a4f97130
Jaiden Nowak <windowsxp519@gmail.com>
2022-02-20 01:06:14 -0500

    Update create.sh

--- Docker/nextspaceos/create.sh
+++ Docker/nextspaceos/create.sh
@@ -4,6 +4,6 @@ docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-dev nextspac
 docker start nextspace-dev
 docker exec -w / -it nextspace-dev git clone https://github.com/WindowsXP95/NeXTSPACE-OS.git
 docker exec -w /NeXTSPACE-OS -it nextspace-dev sh build.sh
-docker cp nextspace-dev:/NeXTSPACE-OS/NEXTSPACEOS.iso ~/nextspaceos_v1.0.3-$(date +"%Y%m%d").iso
+docker cp nextspace-dev:/NeXTSPACE-OS/NEXTSPACEOS.iso ~/nextspaceos_stable-$(date +"%Y%m%d").iso
 docker stop nextspace-dev
 docker container rm nextspace-dev
