88313ab598e3ebe52ae8ffe5a560a0ca8fbe1a62
Jaiden Nowak <windowsxp519@gmail.com>
2022-02-19 20:40:32 -0500

    Renamed iso file

--- testing/build_testing.sh
+++ testing/build_testing.sh
@@ -1,1 +1,1 @@
-livecd-creator --config /NeXTSPACE-OS/testing/nextspace.ks --title "NEXTSPACEOS" --fslabel "NEXTSPACEOS"
+livecd-creator --config /NeXTSPACE-OS/testing/nextspace.ks --title "NEXTSPACEOS_testbuild" --fslabel "NEXTSPACEOS_testbuild"
