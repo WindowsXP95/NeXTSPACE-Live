e5b2885912bad3c307a42e59a6e46aab459134d4
Jaiden Nowak <windowsxp519@gmail.com>
2022-02-26 20:00:24 -0500

    removed timestamp

--- Docker/nextspaceos-testing/create.sh
+++ Docker/nextspaceos-testing/create.sh
@@ -4,6 +4,6 @@ docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-testing next
 docker start nextspace-testing
 docker exec -w / -it nextspace-testing git clone https://github.com/WindowsXP95/NeXTSPACE-OS.git
 docker exec -w /NeXTSPACE-OS -it nextspace-testing sh /NeXTSPACE-OS/testing/build_testing.sh
-docker cp nextspace-testing:/NeXTSPACE-OS/NEXTSPACEOS_testbuild.iso ~/nextspaceos_testbuild-$(date +"%Y%m%d_%H:%M:%S").iso
+docker cp nextspace-testing:/NeXTSPACE-OS/NEXTSPACEOS_testbuild.iso ~/nextspaceos_testbuild-$(date +"%Y%m%d").iso
 docker stop nextspace-testing
 docker container rm nextspace-testing
