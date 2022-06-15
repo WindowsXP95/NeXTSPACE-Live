6359e84598111e8cc8ba6b5987dd57cc2fe3612f
Jaiden Nowak <windowsxp519@gmail.com>
2022-06-15 14:02:37 -0400

    Unifing into one script

--- Docker/nextspaceos-testing/create.sh
+++ Docker/nextspaceos-testing/create.sh
@@ -3,7 +3,7 @@ docker build -t nextspace-testing .
 docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-testing nextspace-testing:latest
 docker start nextspace-testing
 docker exec -w / -it nextspace-testing git clone https://github.com/WindowsXP95/NeXTSPACE-OS.git
-docker exec -w /NeXTSPACE-OS -it nextspace-testing sh /NeXTSPACE-OS/testing/build_testing.sh
+docker exec -w /NeXTSPACE-OS -it nextspace-testing sh /NeXTSPACE-OS/testing/build.sh
 docker cp nextspace-testing:/NeXTSPACE-OS/NEXTSPACEOS_Beta.iso ~/nextspaceos_beta-$(date +"%Y%m%d").iso
 docker stop nextspace-testing
 docker container rm nextspace-testing
