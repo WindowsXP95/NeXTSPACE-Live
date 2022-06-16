ad823b2bb8f4271de6d90addfd44c872258fa01b
Jaiden Nowak <windowsxp519@gmail.com>
2022-06-15 23:05:52 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-testing next
 docker start nextspace-testing
 docker exec -w / -it nextspace-testing git clone https://github.com/WindowsXP95/NeXTSPACE-OS.git
 docker exec -w /NeXTSPACE-OS -it nextspace-testing sh /NeXTSPACE-OS/build/build.sh
-docker cp -R -u -p nextspace-testing:/NeXTSPACE-OS/NEXTSPACEOS.iso ~/nextspaceos_rel-$(date +"%Y%m%d").iso
+docker cp nextspace-testing:/NeXTSPACE-OS/NEXTSPACEOS.iso ~/nextspaceos_rel-$(date +"%Y%m%d").iso
 docker stop nextspace-testing
 docker container rm nextspace-testing
