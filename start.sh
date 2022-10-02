dd4e428d083d053955a7b84fe68914a78df90308
Jaiden Nowak <windowsxp519@gmail.com>
2022-10-02 15:40:19 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # 2022 The NextSpace OS Project
-# Start script v1.1a
+# Start script v1.2a
 
-docker build -t nextspace-testing . -f Dockerfile
-docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-testing nextspace-testing:latest
+docker build -t nextspace-build -f Dockerfile .
+docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-build nextspace-build:latest
 docker start nextspace-testing
-docker exec -w / -it nextspace-testing git clone https://github.com/WindowsXP95/NeXTSPACE-OS.git
-docker exec -w /NeXTSPACE-OS -it nextspace-testing sh /NeXTSPACE-OS/build/build.sh
-docker cp nextspace-testing:/NeXTSPACE-OS/nextspaceos_rel.iso ~/nextspaceos_rel-$(date +"%Y%m%d").iso
-docker cp nextspace-testing:/NeXTSPACE-OS/nextspaceos_rel_beta.iso ~/nextspaceos_rel_beta-$(date +"%Y%m%d").iso
-#docker stop nextspace-testing
-#docker container rm nextspace-testing
+docker exec -w / -it nextspace-build git clone https://github.com/WindowsXP95/NeXTSPACE-OS.git
+docker exec -w /NeXTSPACE-OS -it nextspace-build sh /NeXTSPACE-OS/build/build.sh
+docker cp nextspace-build:/NeXTSPACE-OS/nextspaceos_rel.iso ~/nextspaceos_rel-$(date +"%Y%m%d").iso
+docker cp nextspace-build:/NeXTSPACE-OS/nextspaceos_rel_beta.iso ~/nextspaceos_rel_beta-$(date +"%Y%m%d").iso
+docker stop nextspace-build
+docker container rm nextspace-build
